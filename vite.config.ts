ba7c259de8dfc830ad8507dd4bc804e60edf7460
lares <liang.wang@sincerecloud.com>
2023-07-13 14:23:38 +0800

    兼容浏览器配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url"
 
 import { defineConfig, loadEnv } from "vite"
 import vue from "@vitejs/plugin-vue"
+import legacy from "@vitejs/plugin-legacy"
 
 import AutoImport from "unplugin-auto-import/vite"
 import Components from "unplugin-vue-components/vite"
@@ -76,6 +77,31 @@ export default defineConfig((params) => {
       createSvgIconsPlugin({
         iconDirs: [path.resolve(rootPath, "src/assets/svgs")]
         // symbolId: "[name]"
+      }),
+      // 浏览器低版本兼容配置
+      legacy({
+        targets: ["defaults", "not IE 11"],
+        additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+        renderLegacyChunks: true,
+        polyfills: [
+          "es.symbol",
+          "es.promise",
+          "es.promise.finally",
+          "es/map",
+          "es/set",
+          "es.array.filter",
+          "es.array.for-each",
+          "es.array.flat-map",
+          "es.object.define-properties",
+          "es.object.define-property",
+          "es.object.get-own-property-descriptor",
+          "es.object.get-own-property-descriptors",
+          "es.object.keys",
+          "es.object.to-string",
+          "web.dom-collections.for-each",
+          "esnext.global-this",
+          "esnext.string.match-all"
+        ]
       })
     ],
     resolve: {
@@ -113,6 +139,7 @@ export default defineConfig((params) => {
       }
     },
     build: {
+      target: "es2015",
       cssCodeSplit: true,
       sourcemap: true,
       ...buildOpts
