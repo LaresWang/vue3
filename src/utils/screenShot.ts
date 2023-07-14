dde5d38cf5b05af8118a5e7683e9b9910946dacc
lares <liang.wang@sincerecloud.com>
2023-07-14 16:11:58 +0800

    截图

--- src/utils/screenShot.ts
+++ src/utils/screenShot.ts
@@ -21,29 +21,48 @@ const getImgDataFromVideo = (videoId: string, containerId?: string): string => {
   if (container) {
     const cwidth = container.offsetWidth
     const cheight = container.offsetHeight
-    // video填满container高度
-    const canvasSize = Math.min(cwidth, cheight, video.width)
+    const { width: vwidth, height: vheight } = video
+
+    const crate = cwidth / cheight
+    const vrate = vwidth / vheight
+    // video填满container高度 cheight=vheight
+    const canvasSize = Math.min(cwidth, cheight, vwidth)
     console.log(canvasSize, 33333)
     // 下面是截视口内video的部分 且截正方形
     canvas.height = canvasSize
     canvas.width = canvasSize
     const ctx = canvas.getContext("2d")
 
-    if (video.width < cwidth) {
-      ctx?.drawImage(video.el, 0, 0, video.width, video.height, 0, 0, canvasSize, canvasSize)
-    } else {
-      const vrate = video.width / video.height
-      const sh = canvasSize / vrate
-      if (sh < canvasSize) {
-        const ncanvasSize = sh
-        canvas.height = ncanvasSize
-        canvas.width = ncanvasSize
-      }
-      
-      //  drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh) 剪切图像，并在画布上定位被剪切的部分
-      ctx?.drawImage(video.el, video.width / 2 - cwidth / 2, (video.height - sh) / 2, canvasSize, sh, 0, 0, canvas.width, canvas.height)
-    }
+    //  drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh) 剪切图像，并在画布上定位被剪切的部分
+    // sw/sh 与 dw/dh的比例需要保持一致，否则画布会出现填不满现象
 
+    // 视频宽度小于容器宽度
+    if (vwidth < cwidth) {
+      if (vrate > 1) {
+        // 视频 宽大与高 canvasSize=vheight  canvasSize < vwidth
+        ctx?.drawImage(video.el, (vwidth - canvasSize) / 2, 0, vheight, vheight, 0, 0, canvasSize, canvasSize)
+      } else {
+        // 视频 宽小于等于高 canvasSize=vwidth  canvasSize < vheight
+        ctx?.drawImage(video.el, 0, 0, vwidth, vwidth, 0, 0, canvasSize, canvasSize)
+      }
+    } else {
+      // 视频宽度大于等于容器宽度
+      if (crate > 1) {
+        // 容器 宽大于高 canvasSize=vheight  canvasSize < cwidth
+        ctx?.drawImage(video.el, (cwidth - canvasSize) / 2, 0, vheight, vheight, 0, 0, canvasSize, canvasSize)
+      } else {
+        // 容器 宽小于等于高 canvasSize=cwidth  canvasSize < cheight
+        ctx?.drawImage(video.el, 0, 0, cwidth, cwidth, 0, 0, canvasSize, canvasSize)
+      }
+      // const sh = canvasSize / vrate
+      // if (sh < canvasSize) {
+      //   const ncanvasSize = sh
+      //   canvas.height = ncanvasSize
+      //   canvas.width = ncanvasSize
+      // }
+
+      // ctx?.drawImage(video.el, video.width / 2 - cwidth / 2, (video.height - sh) / 2, canvasSize, sh, 0, 0, canvas.width, canvas.height)
+    }
   } else {
     // 下面是截video全部
     canvas.height = video.height
